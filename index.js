d5b080e00b30dd97c54ee0e4fd4e0005e4616175
dariuskemei <dariuskimutai76@gmail.com>
2024-10-05 14:06:27 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+  
+  writeCards(["Guadalupe", "Ollie", "Aki"], "Anniversary")
+
+  function writeCards(){
+    return [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+    ]
+  }
+
+  function countDown(number){
+    while(number >=0){
+        console.log(number)
+  }
+}
